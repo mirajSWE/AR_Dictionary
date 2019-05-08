2958dffa0fa0f2092293ae484734aa3ccf6a52f3
mirajbrahmbhatt <mirajb19@gmail.com>
2019-05-08 15:10:10 -0500

    Initial Commit

---
+++ AR_Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR_Dictionary
+//
+//  Created by Miraj Brahmbhatt on 5/8/19.
+//  Copyright © 2019 Miraj Brahmbhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

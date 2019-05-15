7fbe1bf71056513b757413f8158dd0b7c95faa1e
mirajbrahmbhatt <mirajb19@gmail.com>
2019-05-14 20:05:20 -0500

    Final

--- AR_Dictionary/ViewController.swift
+++ AR_Dictionary/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import AVKit
 import Vision
 
+
+
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate
 {
+    //@IBOutlet weak var objectIdentity: UILabel!
     
     override func viewDidLoad()
     {
@@ -19,16 +22,13 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         // Start the camera
         let captureCamera = AVCaptureSession()
-        captureCamera.sessionPreset = .photo
+        
+       // captureCamera.sessionPreset = .photo
         
         
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-            return
-        }
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
         
-        guard let cameraInput = try? AVCaptureDeviceInput(device: captureDevice) else {
-            return
-        }
+        guard let cameraInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
         
         captureCamera.addInput(cameraInput)
         
@@ -40,13 +40,38 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 
         previewLayer.frame = view.frame
 
-        
+        //analyze video preview
         let videoDataOutput = AVCaptureVideoDataOutput()
         videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Dispatch Queue"))
         
         captureCamera.addOutput(videoDataOutput)
         
+        
+        
+        
+    }
     
+    func cameraOutput(_ output: AVCaptureOutput, checkOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
+    {
+        guard let pixelBuffer: CVPixelBuffer =
+            CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let mlModel = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        
+        
+        let mlRequest = VNCoreMLRequest(model: mlModel)
+        { (endReqeust, err) in
+            
+            guard let results = endReqeust.results as? [VNClassificationObservation] else { return }
+            guard let initialGuess = results.first else { return }
+            
+            print(initialGuess.identifier, initialGuess.confidence)
+            print("hi")
+            
+            //objectIdentity.text = initialGuess.identitiy
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([mlRequest])
         
     }
     
